f0ca452dca52a0df95fdb7b9f81b20305687e2d2
Asep Jaenudin Sutarji <asepjaenudinsutarji@gmail.com>
2024-05-07 05:11:36 +0700

    add method for register user

--- app/controllers/userController.go
+++ app/controllers/userController.go
@@ -2,13 +2,14 @@ package controllers
 
 import (
 	"e-commerce/app/models"
+	"github.com/google/uuid"
 	render2 "github.com/unrolled/render"
 	"net/http"
 )
 
 func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
 	render := render2.New(render2.Options{
-		Layout:     "layout",
+		Layout:     "layoutLogin",
 		Extensions: []string{".html"},
 	})
 
@@ -33,6 +34,61 @@ func (server *Server) DoLogin(w http.ResponseWriter, r *http.Request) {
 	if !ComparePassword(password, user.Password) {
 		SetFlash(w, r, "error", "Email atau Password tidak sesuai")
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	session, _ := store.Get(r, sessionUser)
+	session.Values["id"] = user.ID
+	_ = session.Save(r, w)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (server *Server) Register(w http.ResponseWriter, r *http.Request) {
+	render := render2.New(render2.Options{
+		Layout:     "layoutLogin",
+		Extensions: []string{".html"},
+	})
+
+	_ = render.HTML(w, http.StatusOK, "register", map[string]interface{}{
+		"error": GetFlash(w, r, "error"),
+	})
+}
+
+func (server *Server) DoRegister(w http.ResponseWriter, r *http.Request) {
+	firstName := r.FormValue("first_name")
+	lastName := r.FormValue("last_name")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	if firstName == "" || lastName == "" || email == "" || password == "" {
+		SetFlash(w, r, "error", "Semua form harus di isi")
+		http.Redirect(w, r, "/register", http.StatusSeeOther)
+		return
+	}
+
+	userModel := models.User{}
+	existUser, _ := userModel.FindByEmail(server.DB, email)
+
+	if existUser != nil {
+		SetFlash(w, r, "error", "email sudah di gunakan")
+		http.Redirect(w, r, "/register", http.StatusSeeOther)
+		return
+	}
+
+	hashedPassword, err := MakePassword(password)
+	params := &models.User{
+		ID:        uuid.New().String(),
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Password:  hashedPassword,
+	}
+
+	user, err := userModel.CreateUser(server.DB, params)
+	if err != nil {
+		SetFlash(w, r, "error", "registrasi gagal")
+		http.Redirect(w, r, "/register", http.StatusSeeOther)
 		return
 	}
 
